9235872f0003dc953c117ef84674930348dbfcb6
Nikhil Sonar <Nikhil.Sonar@teknopoint.local>
2024-02-11 03:53:38 +0530

    added one cofigration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    target: 'es2018', // Or another target that supports CSS nesting syntax
+  },
 })
